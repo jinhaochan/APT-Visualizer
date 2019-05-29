cf993a951d24ceccf5be603d6a98fe6608194bc0
Jin Hao <jinhao_@hotmail.com>
2019-05-29 15:21:28 +0800

    cleaned the tables, dynamically generated modals

--- client/components/mitretable.js
+++ client/components/mitretable.js
@@ -3,19 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Modal from 'react-modal';
 
-import TA0001 from './fragments/TA0001' 
-import TA0002 from './fragments/TA0002' 
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 class MitreTable extends React.Component {
   constructor(...args) {
     super(...args);
 
-    this.state = { TA0001: false,
-	           TA0002: false,
-	           headerData: [],
+    this.state = { headerData: [],
 	           mitreData: []
                  };
 
@@ -41,7 +37,6 @@ fetch(url, {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         headers: {
 	    'Accept': 'application/vnd.oasis.stix+json; version=2.0'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
         },
     })
   .then(response => {
@@ -113,9 +108,6 @@ fetch(url, {
 		    mitreData: this.state.mitreData.concat([impact.data])
          	});
       }));
-
-     this.state.mitreData.map((col, i) => this.state.mitreData.map(row => row[i]))
-
 }
 
 showModal(mitreID){
@@ -141,23 +133,80 @@ render(){
   return (
 	  <div>
 
-          <TA0001
-          show={this.state.TA0001}
-          onHide={() => this.hideModal("TA0001")}
-          />
-          <TA0002
-          show={this.state.TA0002}
-          onHide={() => this.hideModal("TA0002")}
-          />
+          {
+  	    this.state.headerData.map(function(exp) {
+ 		  let id = exp.external_references[0].external_id;
+		  return [
+			  <div>
+			  <Modal
+		        size="lg"
+		        aria-labelledby="contained-modal-title-vcenter"
+		        centered
+			show={this.state[id]} 
+		        >
+		        <Modal.Header>
+		          <Modal.Title id="contained-modal-title-vcenter">
+			  {id}
+		          </Modal.Title>
+		        </Modal.Header>
+		        <Modal.Body>
+		          <h4>Initial Access</h4>
+		          <p>
+		          The initial access tactic represents the vectors adversaries use to gain an initial foothold within a network.
+		          </p>
+		        </Modal.Body>
+		        <Modal.Footer>
+			  <Button onClick={() => this.hideModal(id)}>Close</Button>
+		        </Modal.Footer>
+		      </Modal>
+			</div>
+	            ]
+	    }, this)
+	  }
 
-          <Table striped bordered >
+          {
+  	    this.state.mitreData.map(exp => {
+		  exp.map(data => {
+			  let id = data.external_references[0].external_id;
+			  return [
+				  <div>
+                          <Modal
+                        size="lg"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        centered
+                        show={this.state[id]}
+                        >
+                        <Modal.Header>
+                          <Modal.Title id="contained-modal-title-vcenter">
+                          {id}
+                          </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                          <h4>Initial Access</h4>
+                          <p>
+                          The initial access tactic represents the vectors adversaries use to gain an initial foothold within a network.
+                          </p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                          <Button onClick={() => this.hideModal(id)}>Close</Button>
+                        </Modal.Footer>
+                      </Modal>
+                        </div>
+		            ]
+
+	    }, this)
+	  }, this)
+
+		 }
+
+          <Table striped bordered class="table-hide">
           <thead bgcolor="#d3d3d3">
           <tr>
               {
               this.state.headerData.map(function(exp){
                   return [
 
-                          <th><a class="matrix-tactics-url" href="#" onClick={() => this.showModal(exp.external_references.external_id)}>{exp.name}</a></th>
+                          <th><a class="matrix-tactics-url" href="#" onClick={() => this.showModal(exp.external_references[0].external_id)}>{exp.name}</a></th>
                    ]  
                   }, this)
               }
@@ -169,11 +218,13 @@ render(){
                 return [
                       <tr>
 			{exp.map(function(data){
-				return [
-		        <td class="cell"><a class="technique-mapping" href="#" >{data.name}</a></td>
-				]}, this)
+				return data != 0 ?
+		        <td><a class="technique-mapping" href="#" onClick={() => this.showModal(data.external_references[0].external_id)}>{data.name}</a></td>
+				:
+					<td style={{border:'none', visibility:'hidden'}}></td>
+				}, this)
 			}
-                     </tr>
+			</tr>
 		]
               }, this)
 	  }
